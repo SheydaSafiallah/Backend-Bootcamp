e082af16fccd4d7c455b0fd562ba8e319dd0db52
Sheyda Safiallah <sheyda.safiallah@gmail.com>
2023-05-03 02:44:06 +0330

    i think i do it bad!

--- userRoutes.js
+++ userRoutes.js
@@ -2,5 +2,5 @@ const express = require('express')
 const authController = require('./../Backend Bootcamp/authController')
 const router = express.router();
 
-router.route('/').post('/signup', authController.signup)
+router.post('/signup', authController.signup)
 module.exports = router;
