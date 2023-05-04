ef6c4206471029eb3cec2fee05c33c02ed77077c
Sheyda Safiallah <sheyda.safiallah@gmail.com>
2023-05-04 08:34:53 +0330

    deletion

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const port = 5000;
-
-const app = express();
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
-
--- userRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const authController = require('./../Backend Bootcamp/authController')
-const router = express.router();
-
-router.post('/signup', authController.signup)
-module.exports = router;
