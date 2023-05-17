80a7bb83d8ee02db2e5ece2d1e13b9f999cd27ca
Sheyda Safiallah <sheyda.safiallah@gmail.com>
2023-05-17 17:48:32 +0330

    Modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 const http = require('http');
 const url = require('url')
+const slugify = require('slugify') 
+
+const replaceTemplate = require('./modules/replaceTemplate') 
+
 
 /////////////////////////////////////FILE
 //blocking, synchronous way
@@ -104,21 +108,6 @@ const url = require('url')
 // })
 
 ///////////////// load file once use it many times!
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    output = output.replace(/{%ID%}/g, product.id)
-
-    if(! product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    return output
-
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html` , 'utf-8')
 const tempcard = fs.readFileSync(`${__dirname}/templates/template-card.html` , 'utf-8')
 const tempproduct = fs.readFileSync(`${__dirname}/templates/template-product.html` , 'utf-8')
