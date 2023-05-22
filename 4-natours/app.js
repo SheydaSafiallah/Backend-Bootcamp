23d00fb1627fb3bc69c11619f35d4b6a4e9bf52f
Sheyda Safiallah <sheyda.safiallah@gmail.com>
2023-05-22 21:32:50 +0330

    express experience!

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,25 @@
+const fs = require('fs') 
+const morgan =  require('morgan')
 const express = require('express');
 
 const app = express();
+//// logging middleware
+app.use(morgan('dev'))
+
+app.use(express.json())
+
+////middlewares
+// app.use((req, res, next)=> {
+//     console.log('hello from middleware')
+//     next()
+// })
+
+// app.use((req, res, next)=> {
+//     req.requestTime = new Date().toISOString()
+//     next()
+// })
+/////////////////////////////////////////////////////////////////
+
 // send string to server
 // app.get('/', (req, res) => {   
 //     res.status(200).send('hello from serev side');
@@ -8,17 +27,137 @@ const app = express();
 
 
 //  send json to server -> HTTP method , get method
-app.get('/', (req, res) => {   
-    res.status(200).json({message: 'hello from serev side', app: 'NATOURS'});
+// app.get('/', (req, res) => {   
+//     res.status(200).json({message: 'hello from serev side', app: 'NATOURS'});
+// })
+
+// first api -> get
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json(
+        {
+            status: 'OK',
+            results: tours.length,
+            data: {
+                tours
+            }
+        }
+    );
+})
+
+///// get the id of data from url path
+/// /:id/:x/:y/:z? -> last one is optional
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params)
+    const id = req.params.id * 1
+    const tour = tours.find(el => el.id === id)
+
+    // if (id > tours.length)
+    if (! tour){
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }
+
+    res.status(200).json({
+        status: "success",
+        data : {
+            tour
+        }
+    })
+
+
+
 })
 
 
-app.post('/', (req, res) =>{
-    res.json({message: 'you can post to this endpoint....', app: 'NATOURS' });
+/// post
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    const newID = tours[tours.length-1].id + 1
+    const newTour = Object.assign({id: newID}, req.body)
+    tours.push(newTour)
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json` ,
+        JSON.stringify(tours),
+        err =>{
+            res.status(201).json({
+                status: 'success',
+                data : {
+                    tours: newTour
+                }
+            })
+        } )
 })
+
+
+
+
+// app.post('/', (req, res) =>{
+//     res.json({message: 'you can post to this endpoint....', app: 'NATOURS' });
+// })
 
 
 const port = 3000;
 app.listen(port, ()=>{
     console.log('listening on port  ' + port);
 })
+
+
+
+/////////////////////////////////////////////////////refactoring///////////////////////////////////////////////
+ ///// we have this
+
+// app.get('/api/v1/tours', (req, res) => {
+//     res.status(200).json(
+//         {
+//             status: 'OK',
+//             results: tours.length,
+//             data: {
+//                 tours
+//             }
+//         }
+//     );
+// })
+
+
+/// changed to this
+
+// const getAllTours = (req, res) => {
+//     res.status(200).json(
+//         {
+//             status: 'OK',
+//             results: tours.length,
+//             data: {
+//                 tours
+//             }
+//         }
+//     );
+// }
+
+// app.get('/api/v1/tours',getAllTours)
+
+/////////////// and now route!!!
+
+// when we make changes like above, we have s.th like this:
+//app.get('/api/v1/tours',getAllTours)
+//app.get('/api/v1/tours/:id',getTour)
+//app.get('/api/v1/tours/:id',updateTour)
+//app.get('/api/v1/tours/:id',deleteTour)
+//app.get('/api/v1/tours',createTour)
+
+/// we can refactor it again!!!
+// like these function:
+// app
+//     .route('/api/v1/tours')
+//     .get(getAllTours)
+//     .post(createTour)
+
+// app
+//    .route('/api/v1/tours/:id')
+//    .patch(updateTour)
+//    .delete(deleteTour)
